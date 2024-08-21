d26c18ffd3bcc737d19e584f8206e45811ffa670
Insham Haque <haqueinsham@gmail.com>
2024-08-21 15:55:41 +0530

    added tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import logo from '../assets/logo.png'
+export const Header = ()=>{
+    return(
+        <div className="flex flex-row justify-between bg-gray-900 border-b border-gray-300">
+            
+        </div>
+    )
+}
